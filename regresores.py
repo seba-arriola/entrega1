f0b28796210e5362c1c0df9d83b1ecfc8ea5b0cf
Seba Arriola <sarriola@ing.uchile.cl>
2019-07-03 17:08:47 -0400

    entrega final

--- regresores.py
+++ regresores.py
@@ -226,3 +226,188 @@ plt.xlabel('Datos')
 plt.ylabel('Tiempo [s]')
 plt.legend(['predicciones', 'datos'], loc='upper left')
 plt.show()
+
+
+####################################################################################################
+# SEGUNDA PARTE
+####################################################################################################
+
+data_S = pd.read_csv('lecturas_S.dat', sep=" ", header=None, names=["lat_hip","lon_hip","prof_hip","sta_name","lat_sta","lon_sta","elev_sta","t_s" ])
+
+data_S = data_S.drop(columns="sta_name")
+
+data_S.describe()
+data_S.info()
+
+
+features_S = data_S.loc[:, :"elev_sta"]
+
+y_S = data_S["t_s"]
+
+
+X_train_S, X_test_S, y_train_S, y_test_S = train_test_split(features_S, y_S, test_size=0.1,random_state=42)
+
+
+scaler_S = StandardScaler()
+scaler_S.fit(features_S)  
+transformed_X_S = scaler_S.transform(features_S)
+
+
+
+classifier1 = SGDRegressor(loss="huber", penalty="elasticnet",max_iter=1000, tol=1e-4)
+
+scoring=['explained_variance','neg_mean_absolute_error','neg_mean_squared_error','neg_mean_squared_log_error','neg_median_absolute_error','r2']
+
+cv_results = cross_validate(classifier1,transformed_X_S, y_S, cv = 10, n_jobs=10, scoring = scoring, return_train_score= True)
+
+print('explained_variance:', np.mean(cv_results['test_explained_variance']))
+print('neg_mean_absolute_error:', np.mean(cv_results['test_neg_mean_absolute_error']))
+print('neg_mean_squared_error:', np.mean(cv_results['test_neg_mean_squared_error']))
+print('neg_mean_squared_log_error:', np.mean(cv_results['test_neg_mean_squared_log_error']))
+print('neg_median_absolute_error:', np.mean(cv_results['test_neg_median_absolute_error']))
+print('r2:', np.mean(cv_results['test_r2']))
+
+
+
+classifier1 = SGDRegressor(loss="huber", penalty="elasticnet",max_iter=1000, tol=1e-4)
+
+scaler2_S = StandardScaler()
+scaler2_S.fit(X_train_S)  
+transformed_X_train_S = scaler2_S.transform(X_train_S)
+transformed_X_test_S = scaler2_S.transform(X_test_S)
+
+classifier1.fit(transformed_X_train_S,y_train_S)
+
+preds1_S = classifier1.predict(transformed_X_test_S)
+
+print("score: %.5f" %(classifier1.score(transformed_X_test_S,y_test_S)))
+
+print("Error cuadratico medio: %.5f" % mean_squared_error( y_test_S, preds1_S ))
+
+
+y_reg_S = y_test_S.tolist()
+
+plt.rcParams["figure.figsize"] = (20,10)
+plt.plot(preds1_S[1:500])
+plt.plot(y_reg_S[1:500])
+plt.xlabel('Datos')
+plt.ylabel('Tiempo [s]')
+plt.legend(['predicciones', 'datos'], loc='upper left')
+plt.show()
+
+
+# Kernel Gaussiano
+rbf_kernel = RBF(length_scale=10)  
+ker_regr_rbf = KernelRidge(kernel=rbf_kernel)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+cv_results2 = cross_validate(ker_regr_rbf,transformed_X_S, y_S, cv = 10, scoring = scoring, return_train_score= True)
+
+print('explained_variance:', np.mean(cv_results2['test_explained_variance']))
+print('neg_mean_absolute_error:', np.mean(cv_results2['test_neg_mean_absolute_error']))
+print('neg_mean_squared_error:', np.mean(cv_results2['test_neg_mean_squared_error']))
+print('neg_mean_squared_log_error:', np.mean(cv_results2['test_neg_mean_squared_log_error']))
+print('neg_median_absolute_error:', np.mean(cv_results2['test_neg_median_absolute_error']))
+print('r2:', np.mean(cv_results['test_r2']))
+
+
+datos_S = data_S.values
+datos_X_S = datos_S[:,:-1].tolist()
+datos_Y_S = datos_S[:,-1:].tolist()
+X_train_TF_S, X_test_TF_S, y_train_TF_S, y_test_TF_S = train_test_split(datos_X_S, datos_Y_S, test_size=0.1,random_state=42)
+
+scaler3_S = StandardScaler()
+scaler3_S.fit(X_train_TF_S)  
+transformed_X_train_TF_S = scaler3_S.transform(X_train_TF_S)
+transformed_X_test_TF_S = scaler3_S.transform(X_test_TF_S)
+
+tf.reset_default_graph()
+
+input_ = tflearn.input_data(shape=[None,6])
+
+r1 = tflearn.fully_connected(input_,6)
+r1 = tflearn.fully_connected(r1,1)
+r1 = tflearn.regression(r1, optimizer='adam', loss='mean_square')
+
+m = tflearn.DNN(r1)
+m.fit(transformed_X_train_TF_S,y_train_TF_S, n_epoch=50, show_metric=True, snapshot_epoch=False)
+DNN_pred_S = m.predict(transformed_X_test_TF_S)
+print("score: %.5f" %( r2_score( y_test_TF_S, DNN_pred_S ) ))
+print("Error cuadratico medio: %.5f" % mean_squared_error( y_test_TF_S, DNN_pred_S ))
+
+
+
+from sklearn.ensemble import GradientBoostingRegressor
+
+X_train_S, X_test_S, y_train_S, y_test_S = train_test_split(features_S, y_S, test_size=0.1,random_state=42)
+scaler2_S = StandardScaler()
+scaler2_S.fit(X_train_S)  
+transformed_X_train_S = scaler2_S.transform(X_train_S)
+transformed_X_test_S = scaler2_S.transform(X_test_S)
+
+low_coef = 0.1
+up_coef  = 0.9
+
+lower_model = GradientBoostingRegressor(loss="quantile",alpha=low_coef,n_estimators=5000)
+mid_model = GradientBoostingRegressor(loss="ls",n_estimators=5000)
+upper_model = GradientBoostingRegressor(loss="quantile",alpha=up_coef,n_estimators=5000)
+
+scoring=['explained_variance','neg_mean_absolute_error','neg_mean_squared_error','neg_mean_squared_log_error','neg_median_absolute_error','r2']
+
+cv_results = cross_validate(mid_model,features_S, y_S, cv = 10, n_jobs=10, scoring = scoring, return_train_score= True)
+
+print('explained_variance:', np.mean(cv_results['test_explained_variance']))
+print('neg_mean_absolute_error:', np.mean(cv_results['test_neg_mean_absolute_error']))
+print('neg_mean_squared_error:', np.mean(cv_results['test_neg_mean_squared_error']))
+print('neg_mean_squared_log_error:', np.mean(cv_results['test_neg_mean_squared_log_error']))
+print('neg_median_absolute_error:', np.mean(cv_results['test_neg_median_absolute_error']))
+print('r2:', np.mean(cv_results['test_r2']))
+
+
+mid_model.fit(transformed_X_train_S,y_train_S)
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+preds1_S = mid_model.predict(transformed_X_test_S)
+
+print("score: %.5f" %(mid_model.score(transformed_X_test_S,y_test_S)))
+
+print("Error cuadratico medio: %.5f" % mean_squared_error( y_test_S, preds1_S ))
+
+lower_model.fit(transformed_X_train_S,y_train_S)
+upper_model.fit(transformed_X_train_S,y_train_S)
+
+predictions_lower = lower_model.predict(transformed_X_test_S)
+predictions_mid   = mid_model.predict(transformed_X_test_S)
+predictions_upper = upper_model.predict(transformed_X_test_S)
+
+predictions = y_test_S.tolist()
+n_init=0
+N_sam = 150
+x=np.linspace(n_init+1,N_sam+n_init,N_sam)
+
+plt.rcParams["figure.figsize"] = (20,10)
+plt.plot(x,predictions[n_init:N_sam+n_init],linewidth=2.0, color='red')
+plt.plot(x,predictions_mid[n_init:N_sam+n_init],linewidth=1.0, color='blue')
+plt.plot(x,predictions_lower[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.plot(x,predictions_upper[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.fill_between(x, predictions_lower[n_init:N_sam+n_init],predictions_upper[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.legend(['Datos', 'predicción','Rango']);
+plt.show()
+
+predictions = y_test_S.tolist()
+n_init=151
+N_sam = 150
+x=np.linspace(n_init+1,N_sam+n_init,N_sam)
+
+plt.rcParams["figure.figsize"] = (20,10)
+plt.plot(x,predictions[n_init:N_sam+n_init],linewidth=2.0, color='red')
+plt.plot(x,predictions_mid[n_init:N_sam+n_init],linewidth=1.0, color='blue')
+plt.plot(x,predictions_lower[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.plot(x,predictions_upper[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.fill_between(x, predictions_lower[n_init:N_sam+n_init],predictions_upper[n_init:N_sam+n_init], color='gray',alpha=0.4)
+plt.legend(['Datos', 'predicción','Rango']);
+plt.show()
+
+
+
+
